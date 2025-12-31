2523de178458b9d2e343568f45e9fef054cd66c7
SPKUMAR CHALLA <shanmukhapadmakumar.challa@gmail.com>
2025-12-31 16:31:54 +0530

    Delete backend directory

--- backend/routes/geminiRoutes.js
+++
@@ -1,8 +0,0 @@
-import { generateJsonFromText } from "../controller/geminiController.js";
-
-import express from "express";
-const geminiRouter = express.Router();
-
-geminiRouter.post("/generateJsonText", express.text(), generateJsonFromText);
-
-export default geminiRouter;
--- backend/utils/dateUtils.js
+++
@@ -1,4 +0,0 @@
-export const getISTDate = ()=>{
-    const now = new Date();
-    return new Date(now.getTime() + 5.5 *60 *60* 1000);
-};
