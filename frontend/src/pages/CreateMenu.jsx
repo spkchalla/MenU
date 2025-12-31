400ecf82e3eae5825a69d6f8591dc000fcc97e99
SPKUMAR CHALLA <shanmukhapadmakumar.challa@gmail.com>
2025-12-31 16:32:10 +0530

    Delete frontend directory

--- frontend/src/pages/CreateMenu.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CreateMenu = () => {
-  return (
-    <div>CreateMenu</div>
-  )
-}
--- frontend/src/pages/UpdateMenu.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const UpdateMenu = () => {
-  return (
-    <div>UpdateMenu</div>
-  )
-}
