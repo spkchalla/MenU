b67ab2427edcc06ee985ae2d22d04df6627e93bd
spkchalla <shanmukhapadmakumar.challa@gmail.com>
2026-02-10 15:01:28 +0530

    changed cookies

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -57,7 +57,7 @@ export const googleCallback = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/"
     });
