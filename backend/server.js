db3e1bb4a9930d7c28a2385422555ce81d339167
Mohamed <ramy.adam33@gmail.com>
2026-02-16 17:31:07 +0400

    refactor(server): log port number dynamically in server startup

--- backend/server.js
+++ backend/server.js
@@ -114,7 +114,7 @@ process.on("unhandledRejection", (err) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port 5000");
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
